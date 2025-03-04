f8415778d4dba06cdb5d60d4ec52961fed6802e8
Brion <bnanzer@pm.me>
2025-03-04 16:16:50 -0300

    Pruebas de shaders, selector de nivel aleatorio

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -3,11 +3,33 @@ extends Node
 var player: PlayerMovement
 var current_level: Node
 var next_level: Node
+var current_level_path : String = "res://scenes/main.tscn"
+var next_level_path : String
+var levels := []
 
-func load_next_scene(next_scene_pack : PackedScene):
-	var next_level_scene = next_scene_pack.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
-	next_level.add_child(next_level_scene)
-	
+func _ready() -> void:
+	load_levels("res://scenes/levels/")
+	select_next_level()
+
+func load_levels(folder_path: String):
+	var dir = DirAccess.open(folder_path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir():
+				if file_name.ends_with(".tscn"):
+					var file_path = folder_path + file_name
+					levels.append(file_path)
+			file_name = dir.get_next()
+	print("Loaded levels: ", levels)
+
+func load_next_scene():
+	print("Loading scene: " + next_level_path)
+	var next_level_scene : PackedScene = load(next_level_path)
+	var next_level_instance = next_level_scene.instantiate(PackedScene.GEN_EDIT_STATE_INSTANCE)
+	next_level.add_child(next_level_instance)
+
 	var exit_transition_hallway : Node3D = current_level.get_child(0).get_node(GlobalConstants.EXIT_HALLWAY_NAME)
 	var entry_transition_hallway : Node3D = next_level.get_child(0).get_node(GlobalConstants.ENTRY_HALLWAY_NAME)
 	
@@ -20,3 +42,13 @@ func load_next_scene(next_scene_pack : PackedScene):
 func unload_previous_scene():
 	current_level.get_child(0).queue_free()
 	next_level.get_child(0).reparent(current_level)
+	current_level_path = next_level_path
+	select_next_level()
+
+func select_next_level():
+	var random_index = randi() % levels.size()
+	next_level_path = levels[random_index]
+	while next_level_path == current_level_path:
+		random_index = randi() % levels.size()
+		next_level_path = levels[random_index]
+	print("Next level selected: " + next_level_path)
--- scripts/TransitionHallway.gd
+++ scripts/TransitionHallway.gd
@@ -1,13 +1,5 @@
 extends Node3D
 
-@export var next_scene : PackedScene
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	if not next_scene:
-		push_error("Next scene is undefined!")
-
-
 func _on_scene_load_triggered(body: Node3D) -> void:
-	if body.name == GlobalConstants.PLAYER_NAME and next_scene:
-		GameManager.load_next_scene(next_scene)
+	if body.name == GlobalConstants.PLAYER_NAME:
+		GameManager.load_next_scene()
